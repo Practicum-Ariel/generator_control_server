8ba5f861f68236e3b55fefe06ccfe276ae92e23b
yosefus <yosalsoni@gmail.com>
2024-06-23 12:40:39 +0300

    error handle example

--- routes/test.router.js
+++ routes/test.router.js
@@ -3,7 +3,12 @@ const router = express.Router()
 
 
 router.all('/', (req, res) => {
-   res.send('test')
+   try {
+      res.send('test')
+   } catch (error) {
+      console.log('error test', error);
+      res.status(error.status || 500).send(error)
+   }
 })
 
 module.exports = router

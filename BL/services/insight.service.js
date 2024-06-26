27189ba96c651055f8de63dbf72aa3af09f5e00f
sagi buta <sagibuta@gmail.com>
2024-06-27 00:58:27 +0300

    improvment router & clean code

--- BL/services/insight.service.js
+++ BL/services/insight.service.js
@@ -1,18 +1,25 @@
+//sagiButa
+//ai service for insight
+
 const { read, readOne, create, update, del } = require('../../DL/controllers/insight.controller');
 
 // === BL-FUNCTIONS ===
 
-async function getAll(filter, select) {
-
+// --- GETS ALL &
+async function getAllInsights(filter, select) {
     return await read(filter, select);
 }
-async function getInsight(filter) {
 
-    return await readOne(filter);
+// --- GET ONE FUNCTION ---
+async function getOneInsight(filter, select) {
+    filter = { _id: filter }
+    return await readOne(filter, select);
 }
+
+// ---CREATE A NEW INSIGHTS OR ONES---
 async function createInsight(dataArray) {
     // אם הנתונים הם מערך, הכנס כל תובנה בנפרד למסד הנתונים
-    if (Array.isArray(dataArray)) {
+    if (dataArray.length > 0) {
         const results = [];
         for (const data of dataArray) {
             const result = await create(data);
@@ -25,14 +32,14 @@ async function createInsight(dataArray) {
     }
 }
 
+// --- UPDATE INFORMATION VALUES OR SOFT DLETED (ISACTIVE:FALSE) ---
 async function updateInsight(id, data) {
-
     return await update(id, data);
 }
 
+// ---DELETED  AN INSIGHT ---
 async function deleteInsight(id) {
-
     return await del(id);
 }
 
-module.exports = { createInsight, getInsight, getAll, updateInsight, deleteInsight };
+module.exports = { createInsight, getOneInsight, getAllInsights, updateInsight, deleteInsight };
--- routes/insightsAiServer.router.js
+++ routes/insightsAiServer.router.js
@@ -1,35 +1,22 @@
+//sagiButa
+//ai router for insight api
+
 const express = require('express');
 const router = express.Router();
-const { createInsight, getInsight, getAll, updateInsight, deleteInsight } = require('../BL/services/insight.service');
-
-// router.get('/', async (req, res) => {
-//     try {
-//         const Insight = await getAll();
-//         res.send(Insight);
-//     } catch (err) {
-//         res.status(400).send(err.message);
-//     }
-// });
-// router.delete('/:Insight', async (req, res) => {
-//     try {
-//         const Insight = await deleteInsight(req.params.id);
-//         if (!Insight) {
-//             return res.status(404).send('Insight not found');
-//         }
-//         res.status(200).send('Insight deleted');
-//     } catch (err) {
-//         res.send(err.message);
-//     }
-// });
-
-
+const { createInsight, getOneInsight, getAllInsights, updateInsight, deleteInsight } = require('../BL/services/insight.service');
 
 // Get all insights by any filter
 router.get('/', async (req, res) => {
     try {
-        const filter = req.query;
-        const select = req.query.select || null;
-        const insights = await getAll(filter, select);
+        let filter = {}
+        const select = req.query.select || null;// ? optional select to return
+        if (req.query.filter) {
+            const filterVlue = req.query.filter, // any filter no unique
+                key = req.query.key
+            // filter = { [key]: filterVlue }
+            filter[key] = filterVlue
+        }
+        const insights = await getAllInsights(filter, select);
         res.status(200).json(insights);
     } catch (err) {
         res.status(400).send(err.message);
@@ -37,10 +24,16 @@ router.get('/', async (req, res) => {
 });
 
 // Get one insight by _id or any filter
-router.get('/:oneinsight', async (req, res) => {
+router.get('/onebyid', async (req, res) => {
     try {
-        const filter = req.params.filter
-        const insight = await getInsight(filter);
+        const filterId = req.query.id;// id
+        if (!filterId) {
+            res.status(400).send('Filter parameter id is required');
+            return false;
+        }
+        // if (!filter) { return res.status(400).send('Filter parameter id is required'); }
+        const select = req.query.select || null; // ? optional select to return
+        const insight = await getOneInsight(filterId, select);
         if (!insight) {
             return res.status(404).send('Insight not found');
         }
@@ -50,7 +43,6 @@ router.get('/:oneinsight', async (req, res) => {
     }
 });
 
-// Create a new insight
 router.post('/', async (req, res) => {
     try {
         const data = await createInsight(req.body);
@@ -85,10 +77,5 @@ router.delete('/:id', async (req, res) => {
         res.status(400).send(err.message);
     }
 });
-
-module.exports = router;
-
-
-
 
 module.exports = router;
